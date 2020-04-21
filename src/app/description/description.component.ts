345e7addae62dae779ec57913224551d4f33877e
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-04-22 00:29:01 +0530

    Description Component updated.

--- src/app/description/description.component.ts
+++ src/app/description/description.component.ts
@@ -1,33 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, Observer } from 'rxjs';
-import { Router } from '@angular/router';
-
-interface ExampleTab{
-  label?:string;
-  content?:any;
-}
+import { Component, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { UniversalService } from "../universal.service";
 
 @Component({
-  selector: 'app-description',
-  templateUrl: './description.component.html',
-  styleUrls: ['./description.component.css']
+  selector: "app-description",
+  templateUrl: "./description.component.html",
+  styleUrls: ["./description.component.css"],
 })
 export class DescriptionComponent implements OnInit {
-  asyncTabs: Observable<ExampleTab[]>;
-  constructor(private router:Router) {
-    this.asyncTabs = new Observable((observer: Observer<ExampleTab[]>) => {
-      setTimeout(() => {
-        observer.next([
-          {label: 'Comics', content: this.router.navigate(['/comic'])},
-          {label: 'Events', content: this.router.navigate(['/event'])},
-          {label: 'Series', content: this.router.navigate(['/series'])},
-          {label: 'Stories', content: this.router.navigate(['/story'])},
-        ]);
-      }, 1000);
-    });
-   }
+  public Data: any;
+  message: any = {};
+  subscription: Subscription;
+  constructor(private universalService: UniversalService) {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
