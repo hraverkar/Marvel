f47a8ca85f63faa2ebe308480c80a9dbc966ed4f
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-04-21 21:33:32 +0530

    Data service to fetch data from marvel.

---
+++ src/app/data.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { catchError, retry, tap } from 'rxjs/operators';
+import { Md5 } from 'ts-md5/dist/md5';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+ // public md5 = new Md5();
+  public URL = 'https://gateway.marvel.com:443/';
+
+  public publicKey = '3098e996e853abd6382fd5b79bf1ea2a';
+  public privateKey = 'f89ce7640981357d159af9b52d2adeb88c859b26';
+  public timestamp = new Date().getTime();
+  public stringToHash = this.timestamp + this.privateKey + this.publicKey;
+  public  hash = Md5.hashStr(this.stringToHash);
+  constructor(private httpClient: HttpClient) {}
+
+  getCharactersData() {
+    return this.httpClient
+      .get<any>(
+        this.URL +
+          'v1/public/characters?ts=' +
+          this.timestamp +
+          '&apikey=' +
+          this.publicKey +
+          '&hash=' +
+          this.hash +
+          '',
+        { params: new HttpParams({}), observe: 'response' }
+      )
+      .pipe(
+        retry(3),
+        catchError(this.handleError),
+        tap((res) => {})
+      );
+  }
+
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknow error!;';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `Error :${error.error.message}`;
+    } else {
+      errorMessage = `Error : ${error.status} \nMessage :${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
+
+
+}
