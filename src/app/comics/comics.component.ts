fb41e7763355dae9304de941beaf3061ff6de45b
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-04-21 21:34:20 +0530

    Comics component added.

---
+++ src/app/comics/comics.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comics',
+  templateUrl: './comics.component.html',
+  styleUrls: ['./comics.component.css']
+})
+export class ComicsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    this.getComicsInfo();
+  }
+
+  getComicsInfo(){
+
+  }
+
+}
