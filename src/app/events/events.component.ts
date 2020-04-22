ba89eba7ed4193da2dca05dd82212c894863e6a7
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-04-22 19:36:55 +0530

    Event component updated.

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-events',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./events.component.css']
 })
 export class EventsComponent implements OnInit {
-
+  @Input() eventsData: any;
+  name:any;
   constructor() { }
 
   ngOnInit() {
+    this.name = this.eventsData;
+    console.table(this.name);
   }
 
 }
