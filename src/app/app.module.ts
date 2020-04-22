13de4dfda2ecaa85ff5f6770bbe561007d4bfbbb
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-04-22 19:35:52 +0530

    App component and app module updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,29 +9,43 @@ import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatDividerModule } from "@angular/material/divider";
-import { HeaderComponent } from './header/header.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { HeaderComponent } from "./header/header.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import {
   MatSnackBarModule,
   MAT_SNACK_BAR_DEFAULT_OPTIONS,
   MAT_SNACK_BAR_DATA,
-} from '@angular/material/snack-bar';
+} from "@angular/material/snack-bar";
 
-import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
-import { DescriptionComponent } from './description/description.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { MatTabsModule } from '@angular/material/tabs';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ComicsComponent } from './comics/comics.component';
-import { EventsComponent } from './events/events.component';
-import { SeriesComponent } from './series/series.component';
-import { StoriesComponent } from './stories/stories.component';
-import { UniversalService } from './universal.service';
-import { DataService } from './data.service';
+import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
+import { DescriptionComponent } from "./description/description.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { MatTabsModule } from "@angular/material/tabs";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ComicsComponent } from "./comics/comics.component";
+import { EventsComponent } from "./events/events.component";
+import { SeriesComponent } from "./series/series.component";
+import { StoriesComponent } from "./stories/stories.component";
+import { DataService } from "./data.service";
+import { MatTableModule } from "@angular/material/table";
+import { FooterComponent } from "./footer/footer.component";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { Ng4LoadingSpinnerModule } from 'ng4-loading-spinner';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, HeaderComponent, DescriptionComponent, NotFoundComponent, ComicsComponent, EventsComponent, SeriesComponent, StoriesComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    DescriptionComponent,
+    NotFoundComponent,
+    ComicsComponent,
+    EventsComponent,
+    SeriesComponent,
+    StoriesComponent,
+    FooterComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -45,14 +59,21 @@ import { DataService } from './data.service';
     RouterModule,
     ReactiveFormsModule,
     MatTabsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatProgressSpinnerModule,
+    Ng4LoadingSpinnerModule
+
+
   ],
-  providers: [UniversalService,DataService,
+  providers: [
+    DataService,
     { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 5000 } },
     {
       provide: MAT_SNACK_BAR_DATA,
       useValue: {},
-    }],
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
