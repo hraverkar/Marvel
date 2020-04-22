d689bb919fd79b73a15542dc68dc30af34f70557
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-04-23 00:45:07 +0530

    Comics Component updated.

--- src/app/comics/comics.component.ts
+++ src/app/comics/comics.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from "@angular/core";
 export class ComicsComponent implements OnInit {
   @Input() comicsData: any;
   name:any;
+  displayedColumns:string[] = ['resourceURI', 'name'];
   constructor() {
   }
   ngOnInit() {
